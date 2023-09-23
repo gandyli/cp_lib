6bace9919d666c13ca12913cc4ab92c70bd63d63
gandyli <andyli20070803@gmail.com>
2023-09-23 12:23:41 +0800

    move mod_inverse to a new header

---
+++ mod_inverse.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "template.hpp"
+
+template <std::unsigned_integral T>
+constexpr std::tuple<std::make_signed_t<T>, std::make_signed_t<T>, T> bezout(T x, T y) {
+    bool swap = x < y;
+    if (swap)
+        std::swap(x, y);
+    if (y == 0) {
+        if (x == 0)
+            return {0, 0, 0};
+        if (swap)
+            return {0, 1, x};
+        return {1, 0, x};
+    }
+    using S = std::make_signed_t<T>;
+    S s0 = 1, s1 = 0, t0 = 0, t1 = 1;
+    loop {
+        auto [q, r] = divmod(x, y);
+        if (r == 0) {
+            if (swap)
+                return {t1, s1, y};
+            return {s1, t1, y};
+        }
+        S s2 = s0 - S(q) * s1, t2 = t0 - S(q) * t1;
+        x = y;
+        y = r;
+        s0 = s1;
+        s1 = s2;
+        t0 = t1;
+        t1 = t2;
+    }
+}
+template <std::unsigned_integral T>
+constexpr T mod_inverse(T x, T m) {
+    auto [s, t, g] = bezout(x, m);
+    assert(g == 1);
+    return s < 0 ? T(s) + m : s;
+}
--- montgomery.hpp
+++ montgomery.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "template.hpp"
+#include "mod_inverse.hpp"
 
 template <typename T, typename U>
 constexpr T power(T a, U b) {
@@ -12,42 +13,6 @@ constexpr T power(T a, U b) {
 }
 
 namespace impl {
-    template <std::unsigned_integral T>
-    constexpr std::tuple<std::make_signed_t<T>, std::make_signed_t<T>, T> bezout(T x, T y) {
-        bool swap = x < y;
-        if (swap)
-            std::swap(x, y);
-        if (y == 0) {
-            if (x == 0)
-                return {0, 0, 0};
-            if (swap)
-                return {0, 1, x};
-            return {1, 0, x};
-        }
-        using S = std::make_signed_t<T>;
-        S s0 = 1, s1 = 0, t0 = 0, t1 = 1;
-        loop {
-            auto [q, r] = divmod(x, y);
-            if (r == 0) {
-                if (swap)
-                    return {t1, s1, y};
-                return {s1, t1, y};
-            }
-            S s2 = s0 - S(q) * s1, t2 = t0 - S(q) * t1;
-            x = y;
-            y = r;
-            s0 = s1;
-            s1 = s2;
-            t0 = t1;
-            t1 = t2;
-        }
-    }
-    template <std::unsigned_integral T>
-    constexpr T mod_inverse(T x, T m) {
-        auto [s, t, g] = bezout(x, m);
-        assert(g == 1);
-        return s < 0 ? T(s) + m : s;
-    }
     template <typename T>
     struct make_double_width {};
     template <>
@@ -182,7 +147,7 @@ public:
         return lhs *= rhs;
     }
     constexpr mint inv() const {
-        return from_raw(mr().reduce(int_double_t(mr().mbase3()) * impl::mod_inverse(_val, mr().mod())));
+        return from_raw(mr().reduce(int_double_t(mr().mbase3()) * mod_inverse(_val, mr().mod())));
     }
     constexpr mint& operator/=(const mint& rhs) { return *this *= rhs.inv(); }
     friend constexpr mint operator/(mint lhs, const mint& rhs) {
