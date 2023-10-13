d6dc773d57a6dfe708c32d1ccfe2eecf415d5583
gandyli <andyli20070803@gmail.com>
2023-10-13 16:14:31 +0800

    add z algorithm

---
+++ is_substring.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "zalgorithm.hpp"
+
+template <typename T>
+bool is_substring(T s, const T& t) {
+    int n = len(s), m = len(t);
+    s.insert(s.end(), all(t));
+    auto z = zalgorithm(s);
+    _for (i, n, n + m)
+        if (z[i] >= n)
+            return true;
+    return false;
+}
---
+++ zalgorithm.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "template.hpp"
+
+vi zalgorithm(const auto& s) {
+    int n = len(s);
+    if (n == 0)
+        return {};
+    vi z(n);
+    z[0] = 0;
+    for (int i = 1, j = 0; i < n; i++) {
+        int& k = z[i];
+        if (j + z[j] > i)
+            k = Min(j + z[j] - i, z[i - j]);
+        while (i + k < n && s[k] == s[i + k])
+            k++;
+        if (j + z[j] < i + z[i])
+            j = i;
+    }
+    z[0] = n;
+    return z;
+}
