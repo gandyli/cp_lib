a282e61fdb36df9dab7b3c7bde870cbfe716f7d4
gandyli <andyli20070803@gmail.com>
2023-10-15 00:48:13 +0800

    add restore path

---
+++ graph/restore-path.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "../template.hpp"
+
+vi restore_path(vi par, int t) {
+    vi ret{t};
+    while (par[ret.back()] != -1)
+        ret.eb(par[ret.back()]);
+    reverse(ret);
+    return ret;
+}
