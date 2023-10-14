d7fdfdf9e7c00b7ecd44f37c99e7279d49f60596
gandyli <andyli20070803@gmail.com>
2023-10-15 00:57:29 +0800

    add floyd

---
+++ graph/floyd.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "base.hpp"
+
+template <typename T>
+Vec<Vec<T>> floyd(const WeightedGraph<T>& g) {
+    const int n = len(g);
+    VECI(dis, n, n, inf<T>);
+    _for (i, n)
+        dis[i][i] = 0;
+    _for (u, n)
+        foreach (v, g[u])
+            chkmin(dis[u][v], v.cost);
+    _for (k, n)
+        _for (i, n)
+            _for (j, n)
+                chkmin(dis[i][j], dis[i][k] + dis[k][j]);
+    return dis;
+}
