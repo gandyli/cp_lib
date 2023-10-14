224c435eb57bdd9cba9f57f0a3df6bdc81405c79
gandyli <andyli20070803@gmail.com>
2023-10-14 13:53:14 +0800

    add topological-sort.hpp

---
+++ graph/topological-sort.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "base.hpp"
+
+vi topologicalSort(auto& g) {
+    int n = len(g);
+    Vec<bool> vis(n), ins(n);
+    vi ret;
+    auto dfs = [&](auto&& dfs, int u) {
+        if (ins[u])
+            return false;
+        if (!vis[u]) {
+            ins[u] = true;
+            foreach (v, g[u])
+                if (!dfs(dfs, v))
+                    return false;
+            vis[u] = true;
+            ret.eb(u);
+            ins[u] = false;
+        }
+        return true;
+    };
+    _for (i, n)
+        if (!vis[i] && !dfs(dfs, i))
+            return {};
+    reverse(ret);
+    return ret;
+}
