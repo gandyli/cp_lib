c818ff270b26b6ac7141919913f5ad715261d53b
gandyli <andyli20070803@gmail.com>
2023-10-17 10:48:30 +0800

    rewrite graph/base

--- graph/base.hpp
+++ graph/base.hpp
@@ -1,91 +1,121 @@
 #pragma once
 #include "../io.hpp"
 
-using UnweightedGraph = vvi;
 template <typename T>
 struct Edge {
-    int src, to;
+    int from, to;
     T cost;
-
-    Edge(int to, T cost): src(-1), to(to), cost(cost) {}
-    Edge(int src, int to, T cost): src(src), to(to), cost(cost) {}
-
-    Edge& operator=(int x) {
-        to = x;
-        return *this;
-    }
-
+    int id;
     operator int() const { return to; }
 };
-template <typename T>
-using Edges = Vec<Edge<T>>;
-template <typename T>
-using WeightedGraph = Vec<Edges<T>>;
 
-UnweightedGraph graph(int n, int m = -1, bool is_directed = false, bool is_1origin = true) {
-    UnweightedGraph g(n);
-    if (m == -1)
-        m = n - 1;
-    _for (m) {
-        dR(int, x, y);
-        if (is_1origin)
-            x--, y--;
-        g[x].eb(y);
-        if (!is_directed)
-            g[y].eb(x);
-    }
-    return g;
-}
+template <typename T = int, bool directed = false>
+struct Graph {
+    using cost_type = T;
+    using edge_type = Edge<cost_type>;
 
-template <typename T>
-WeightedGraph<T> wgraph(int n, int m = -1, bool is_directed = false, bool is_1origin = true) {
-    WeightedGraph<T> g(n);
-    if (m == -1)
-        m = n - 1;
-    _for (m) {
-        dR(int, x, y);
-        dR(T, c);
-        if (is_1origin)
-            x--, y--;
-        g[x].eb(x, y, c);
-        if (!is_directed)
-            g[y].eb(y, x, c);
-    }
-    return g;
-}
+    int n{}, m{};
+    Vec<edge_type> edges;
+    vi indptr;
+    Vec<edge_type> csr_edges;
 
-template <typename T>
-Edges<T> esgraph(int n, int m, int is_weighted = true, bool is_1origin = true) {
-    Edges<T> es;
-    _for (m) {
-        dR(int, x, y);
-        T c;
-        if (is_weighted)
-            io.read(c);
-        else
-            c = 1;
-        if (is_1origin)
-            x--, y--;
-        es.eb(x, y, c);
-    }
-    return es;
-}
+    vi _deg, _indeg, _outdeg;
+    class OutgoingEdges {
+    public:
+        OutgoingEdges(const Graph* G, int l, int r): G(G), l(l), r(r) {}
+        const edge_type* begin() const { return l == r ? nullptr : &G->csr_edges[l]; }
+        const edge_type* end() const { return l == r ? nullptr : &G->csr_edges[r]; }
 
-template <typename T>
-Vec<Vec<T>> adjgraph(int n, int m, T INF, int is_weighted = true, bool is_directed = false, bool is_1origin = true) {
-    VECI(d, n, n, INF);
-    _for (m) {
-        dR(int, x, y);
-        T c;
-        if (is_weighted)
-            io.read(c);
-        else
-            c = 1;
-        if (is_1origin)
-            x--, y--;
-        d[x][y] = c;
-        if (!is_directed)
-            d[y][x] = c;
+    private:
+        const Graph* G;
+        int l, r;
+    };
+    static constexpr bool is_directed() { return directed; }
+    Graph() = default;
+    Graph(int n): n(n) {}
+    void add(int from, int to, cost_type cost = 1, int id = -1) {
+        if (id == -1)
+            id = m;
+        edges.eb(from, to, cost, id), m++;
     }
-    return d;
-}
+    void read_tree(bool w = false, int off = 1) { read_graph(n - 1, w, off); }
+    void read_graph(int m, bool w = false, int off = 1) {
+        edges.reserve(m);
+        _for (m) {
+            dR(int, a, b), a -= off, b -= off;
+            if (!w)
+                add(a, b);
+            else {
+                dR(cost_type, c);
+                add(a, b, c);
+            }
+        }
+        build();
+    }
+    void build() {
+        indptr.assign(n + 1, 0);
+        foreach (e, edges) {
+            indptr[e.from + 1]++;
+            if constexpr (!directed)
+                indptr[e.to + 1]++;
+        }
+        _for (i, n)
+            indptr[i + 1] += indptr[i];
+        auto counter = indptr;
+        csr_edges.resize(indptr.back() + 1);
+        foreach (e, edges) {
+            csr_edges[counter[e.from]++] = e;
+            if constexpr (!directed)
+                csr_edges[counter[e.to]++] = {e.to, e.from, e.cost, e.id};
+        }
+    }
+    OutgoingEdges operator[](int u) const {
+        return {this, indptr[u], indptr[u + 1]};
+    }
+    const vi& Deg() {
+        if (_deg.empty())
+            calc_deg();
+        return _deg;
+    }
+    const vi& In_deg() {
+        if (_indeg.empty())
+            calc_deg_inout();
+        return _indeg;
+    }
+    const vi& Out_deg() {
+        if (_outdeg.empty())
+            calc_deg_inout();
+        return _outdeg;
+    }
+    int deg(int u) { return Deg()[u]; }
+    int in_deg(int u) { return In_deg()[u]; }
+    int out_deg(int u) { return Out_deg()[u]; }
+    Graph reverse() const {
+        assert(directed);
+        Graph g0(n);
+        foreach (e, edges)
+            g0.add(e.to, e.from, e.cost, e.id);
+        g0.build();
+        return g0;
+    }
+
+    void write(IO& io) const {
+        io.writeln("from to cost id");
+        _for (i, n)
+            foreach (e, (*this)[i])
+                io.writeln(e.from, ' ', e.to, ' ', e.cost, ' ', e.id);
+    }
+
+private:
+    void calc_deg() {
+        _deg.resize(n);
+        foreach (e, edges)
+            _deg[e.from]++, _deg[e.to]++;
+    }
+    void calc_deg_inout() {
+        _indeg.resize(n);
+        _outdeg.resize(n);
+        foreach (e, edges)
+            _indeg[e.to]++, _outdeg[e.from]++;
+    }
+};
