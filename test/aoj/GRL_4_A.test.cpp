0c0862c516148943678cc5d0384e8662b30bdfc1
gandyli <andyli20070803@gmail.com>
2023-10-19 23:07:27 +0800

    add test for find_cycle

---
+++ test/aoj/GRL_4_A.test.cpp
@@ -0,0 +1,16 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_4_A"
+
+#include "all.hpp"
+#include "graph/find_cycle.hpp"
+
+int main() {
+    dR(int, n, m);
+    Graph<bool, true> g(n);
+    g.read_graph(m, false, 0);
+    auto cycle = find_cycle(g);
+    if (cycle.vs.empty())
+        writeln(0);
+    else
+        writeln(1);
+    return 0;
+}
