14adfc33fb23a6bb6e50ebc02f484e80249d1955
gandyli <andyli20070803@gmail.com>
2023-10-19 16:25:36 +0800

    add lcs

---
+++ string/longest_common_subsequence.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include "../template.hpp"
+
+int longest_common_subsequence(auto&& a, auto&& b) {
+    int n = len(b);
+    vi d(n + 1);
+    foreach (a, a) {
+        _for_r (i, n)
+            if (a == b[i])
+                chkmax(d[i + 1], d[i] + 1);
+        _for (i, n)
+            chkmax(d[i + 1], d[i]);
+    }
+    return d[n];
+}
+Vec<pi> longest_common_subsequence_result(auto&& a, auto&& b) {
+    int n = len(a), m = len(b);
+    VEC(int, d, n + 1, m + 1);
+    _for (i, n) {
+        d[i + 1] = d[i];
+        _for (j, m) {
+            chkmax(d[i + 1][j + 1], d[i + 1][j]);
+            if (a[i] == b[j])
+                chkmax(d[i + 1][j + 1], d[i][j] + 1);
+        }
+    }
+    Vec<pi> r;
+    int i = n, j = m;
+    while (d[i][j])
+        if (d[i][j] == d[i - 1][j])
+            i--;
+        else if (d[i][j] == d[i][j - 1])
+            j--;
+        else
+            r.eb(--i, --j);
+    reverse(r);
+    return r;
+}
---
+++ test/aoj/ALDS1_10_C.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_10_C"
+
+#include "all.hpp"
+#include "string/longest_common_subsequence.hpp"
+
+void test(auto&& a, auto&& b) {
+    int lcs = longest_common_subsequence(a, b);
+    auto r = longest_common_subsequence_result(a, b);
+    assert(len(r) == lcs);
+    for (auto&& [x, y]: r)
+        assert(a[x] == b[y]);
+    _for (i, lcs - 1) {
+        assert(r[i].first < r[i + 1].first);
+        assert(r[i].second < r[i + 1].second);
+    }
+}
+int main() {
+    multipleTests([&] {
+        dR(str, s, t);
+        test(s, t);
+        writeln(longest_common_subsequence(s, t));
+    });
+    return 0;
+}
