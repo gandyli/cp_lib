501b8256c290fcb49fd749990bd529e327f623cc
gandyli <andyli20070803@gmail.com>
2023-10-19 20:35:33 +0800

    test is_substring

---
+++ test/custom/is_substring.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+
+#include "all.hpp"
+#include "string/is_substring.hpp"
+#include "random/base.hpp"
+
+str gen(int n) {
+    str s;
+    _for (n)
+        s += char('a' + rnd(0, 3));
+    return s;
+}
+void test() {
+    using namespace std::string_literals;
+    _for (1000)
+        _for (n, 1, 10)
+            _for (m, 1, 10) {
+                str s = gen(n), t = gen(m);
+                assert(is_substring(s, t) == (t.find(s) != t.npos));
+            }
+}
+int main() {
+    test();
+
+    dR(int, a, b);
+    writeln(a + b);
+    return 0;
+}
