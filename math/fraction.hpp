5a70792f20c43aed3fd6dc6f9047ae0dfb21274c
gandyli <andyli20070803@gmail.com>
2023-10-20 11:30:18 +0800

    add fraction

---
+++ math/fraction.hpp
@@ -0,0 +1,53 @@
+#include "gcd.hpp"
+
+template <typename T, typename U>
+struct FractionBase {
+    using R = FractionBase;
+    using Key = T;
+    T x{0}, y{1};
+    FractionBase() = default;
+    FractionBase(auto x): FractionBase(x, 1) {}
+    FractionBase(auto x, auto y): x(x), y(y) {
+        if (y < 0)
+            x = -x, y = -y;
+        if (y != 1) {
+            T g = binary_gcd(x, y);
+            if (g)
+                x /= g, y /= g;
+        }
+    }
+    static R from_raw(T _x, T _y) {
+        R r;
+        r.x = _x, r.y = _y;
+        return r;
+    }
+    friend R operator+(R l, R r) {
+        if (l.y == r.y)
+            return {l.x + r.x, l.y};
+        return {l.x * r.y + l.y * r.x, l.y * r.y};
+    }
+    friend R operator-(R l,  R r) {
+        if (l.y == r.y)
+            return {l.x - r.x, l.y};
+        return {l.x * r.y - l.y * r.x, l.y * r.y};
+    }
+    friend R operator*(R l, R r) { return {l.x * r.x, l.y * r.y}; }
+    friend R operator/(R l, R r) { return {l.x * r.y, l.y * r.x}; }
+    R& operator+=(R r) { return *this = *this + r; }
+    R& operator-=(R r) { return *this = *this - r; }
+    R& operator*=(R r) { return *this = *this * r; }
+    R& operator/=(R r) { return *this = *this / r; }
+    R operator-() const { return from_raw(-x, y); }
+    auto operator<=>(R r) const { return U{x} * r.y <=> U{y} * r.x; }
+#ifdef FASTIO
+    IO& read(IO& io) {
+        return io.read(x, y);
+    }
+    void write(IO& io) const {
+        io.write(x);
+        if (x != 0 && y != 1)
+            io.write('/', y);
+    }
+#endif
+};
+using Fraction = FractionBase<i64, i128>;
---
+++ math/gcd.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "../template.hpp"
+
+template <typename T, typename U>
+auto binary_gcd(T _a, U _b) {
+    using CU = make_unsigned_t<std::common_type_t<T, U>>;
+    CU a = _a >= 0 ? _a : -_a, b = _b >= 0 ? _b : -_b;
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+    int i = std::__countr_zero(a);
+    a >>= i;
+    int j = std::__countr_zero(b);
+    b >>= j;
+    int k = min(i, j);
+    loop {
+        if (a > b)
+            swap(a, b);
+        b -= a;
+        if (b == 0)
+            return a << k;
+        b >>= std::__countr_zero(b);
+    }
+}
