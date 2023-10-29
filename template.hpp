4877f66ac256d5fea4cf33f49ef20e68778e16e9
gandyli <andyli20070803@gmail.com>
2023-10-30 01:22:27 +0800

    floor, ceil

--- template.hpp
+++ template.hpp
@@ -32,8 +32,8 @@ using namespace std::ranges;
 #define eb emplace_back
 // NOLINTEND
 
-constexpr auto ceil(auto&& x, auto&& y) { return x > 0 ? (x + y - 1) / y : x / y; }
-constexpr auto floor(auto&& x, auto&& y) { return x > 0 ? x / y : (x - y + 1) / y; }
+constexpr auto floor(auto&& x, auto&& y) { return x / y - (x % y && (x ^ y) < 0); }
+constexpr auto ceil(auto&& x, auto&& y) { return floor(x + y - 1, y); }
 constexpr auto divmod(auto x, auto y) {
     auto&& q = floor(x, y);
     return std::pair{q, x - q * y};
