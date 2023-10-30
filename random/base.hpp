73555509a2e0ae0a578e936e869b74386c10f35b
gandyli <andyli20070803@gmail.com>
2023-10-30 20:48:27 +0800

    fix random/base

--- random/base.hpp
+++ random/base.hpp
@@ -5,7 +5,7 @@ u64 rnd64() {
     u64& s = seed();
     return s ^= s << 7, s ^= s >> 9;
 }
-auto rnd(auto l, auto r) {
+i64 rnd(i64 l, i64 r) {
     return l + rnd64() % (r - l);
 }
 u32 rnd() { return rnd64(); }
