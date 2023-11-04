90e254f48080b6ddcb932f08661b88a203341e02
gandyli <andyli20070803@gmail.com>
2023-11-04 22:36:05 +0800

    better FOR

--- template.hpp
+++ template.hpp
@@ -5,11 +5,11 @@ using namespace std::ranges;
 // NOLINTBEGIN
 #define FOR1(a) for (std::decay_t<decltype(a)> _ = 0; _ < (a); _++)
 #define FOR2(i, a) for (std::decay_t<decltype(a)> i = 0; i < (a); i++)
-#define FOR3(i, a, b) for (auto i = a; i < (b); i++)
-#define FOR4(i, a, b, c) for (auto i = a; i < (b); i += (c))
-#define FOR1_R(a) for (auto i = (a)-1; i >= 0; i--)
-#define FOR2_R(i, a) for (auto i = (a)-1; i >= 0; i--)
-#define FOR3_R(i, a, b) for (auto i = (b)-1; i >= (a); i--)
+#define FOR3(i, a, b) for (auto i = (a); i < (b); i++)
+#define FOR4(i, a, b, c) for (auto i = (a); i < (b); i += (c))
+#define FOR1_R(a) FOR2_R(i, a)
+#define FOR2_R(i, a) for (auto i = (a); i--;)
+#define FOR3_R(i, a, b) for (auto i = (b); i-- > (a);)
 #define overload4(a, b, c, d, e, ...) e
 #define overload3(a, b, c, d, ...) d
 #define _for(...) overload4(__VA_ARGS__, FOR4, FOR3, FOR2, FOR1)(__VA_ARGS__)
