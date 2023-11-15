76e41281ae4bc91c087c45a3f06e3ec5c7ff75ef
gandyli <andyli20070803@gmail.com>
2023-11-15 14:59:52 +0800

    arbitrary mint & barrett reduction update

--- modint/arbitrary_mint.hpp
+++ modint/arbitrary_mint.hpp
@@ -3,9 +3,12 @@
 #include "math/mod_inverse.hpp"
 #include "math/power.hpp"
 
-template <int id>
+template <typename T, u32 id = 0>
 struct ArbitraryModIntBase {
     using mint = ArbitraryModIntBase;
+    using int_type = T;
+
+    static Barrett<T> bt;
 
     ArbitraryModIntBase() = default;
     ArbitraryModIntBase(Signed auto y) {
@@ -13,6 +16,8 @@ struct ArbitraryModIntBase {
         x = y < 0 ? y + mod() : y;
     }
     ArbitraryModIntBase(Unsigned auto y) { x = y % mod(); }
+    T val() const { return x; }
+    static T mod() { return bt.mod(); }
     mint& operator++() {
         x++;
         if (x == mod())
@@ -48,51 +53,42 @@ struct ArbitraryModIntBase {
         return *this;
     }
     mint& operator*=(const mint& p) {
-        x = rem(u64(x) * p.x);
+        x = bt.mul(x, p.x);
         return *this;
     }
+    mint inv() const { return from_raw(mod_inverse(x, mod())); }
     mint& operator/=(const mint& p) { return *this *= p.inv(); }
     mint operator+() const { return *this; }
-    mint operator-() const { return mint(-x); }
+    mint operator-() const { return from_raw(!x ? 0 : mod() * 2 - x); }
     friend mint operator+(mint lhs, const mint& rhs) { return lhs += rhs; }
     friend mint operator-(mint lhs, const mint& rhs) { return lhs -= rhs; }
     friend mint operator*(mint lhs, const mint& rhs) { return lhs *= rhs; }
     friend mint operator/(mint lhs, const mint& rhs) { return lhs /= rhs; }
     auto operator<=>(const mint& rhs) const = default;
-    mint inv() const { return from_raw(mod_inverse<u32>(x, mod())); }
-    int val() const { return x; }
-    static mint from_raw(int x) {
+
+    static mint from_raw(T x) {
         mint r;
         r.x = x;
         return r;
     }
-    int raw() const { return x; }
+    T raw() const { return x; }
 #ifdef FASTIO
     void read(IO& io) {
-        static int x;
+        static T x;
         io.read(x);
         *this = x;
     }
     void write(IO& io) const { io.write(x); }
 #endif
-    static int& mod() {
-        static int mod = 0;
-        return mod;
-    }
-    static void set_mod(int p) {
-        ASSERT(0 < p && p <= (1 << 30) - 1);
-        mod() = p;
-        barrett() = Barrett(p);
+    static void set_mod(T p) {
+        ASSERT(0 < p && p <= std::numeric_limits<T>::max() / 4);
+        bt = p;
     }
 
 private:
-    int x{};
-
-    u32 rem(u64 p) { return barrett().rem(p); }
-    static Barrett& barrett() {
-        static Barrett b;
-        return b;
-    }
+    T x{};
 };
-
-using ArbitraryModInt = ArbitraryModIntBase<-1>;
+template <typename T, u32 id>
+Barrett<T> ArbitraryModIntBase<T, id>::bt;
+using dmint32 = ArbitraryModIntBase<u32, 0>;
+using dmint64 = ArbitraryModIntBase<u64, 0>;
--- modint/barrett_reduction.hpp
+++ modint/barrett_reduction.hpp
@@ -1,36 +1,56 @@
 #pragma once
 #include "template.hpp"
 
-struct Barrett {
+template <typename T>
+class Barrett;
+
+template <>
+class Barrett<u32> {
+    constexpr auto get(u64 n) const {
+        u64 x = u128(n) * im >> 64;
+        u64 y = x * m;
+        return std::pair(x, y);
+    }
+
+public:
     u32 m{};
     u64 im{};
-    Barrett() = default;
-    Barrett(int n): m(n), im(u64(-1) / m + 1) {}
-    constexpr i64 quo(u64 n) const {
-        u64 x = u64((u128(n) * im) >> 64);
-        u32 r = n - x * m;
-        return m <= r ? x - 1 : x;
+    constexpr Barrett(u32 m = 1): m(m), im(u64(-1) / m + 1) {}
+    constexpr u32 mod() const { return m; }
+    constexpr u64 div(u64 n) const {
+        auto [x, y] = get(n);
+        return x - (n < y);
     }
-    constexpr i64 rem(u64 n) const {
-        u64 x = u64((u128(n) * im) >> 64);
-        u32 r = n - x * m;
-        return m <= r ? r + m : r;
+    constexpr u32 mod(u64 n) const {
+        auto [x, y] = get(n);
+        return n - y + (n < y ? m : 0);
     }
-    constexpr std::pair<i64, int> quorem(u64 n) const {
-        u64 x = u64((u128(n) * im) >> 64);
-        u32 r = n - x * m;
-        if (m <= r)
-            return {x - 1, r + m};
-        return {x, r};
+    constexpr std::pair<u64, u32> divmod(u64 n) const {
+        auto [x, y] = get(n);
+        if (n < y)
+            return {x - 1, n - y + m};
+        return {x, n - y};
     }
-    constexpr i64 pow(u64 n, i64 p) const {
-        u32 a = rem(n), r = m != 1;
-        while (p) {
-            if (p & 1)
-                r = rem(u64(r) * a);
-            a = rem(u64(a) * a);
-            p >>= 1;
-        }
-        return r;
+    constexpr u32 mul(u32 x, u32 y) const { return mod(u64(x) * y); }
+};
+template <>
+class Barrett<u64> {
+public:
+    u64 m{}, imh{}, iml{};
+    constexpr Barrett(u64 m = 1): m(m) {
+        u128 im = u128(-1) / m;
+        if (im * m + m == 0)
+            im++;
+        imh = im >> 64;
+        iml = im;
     }
+    constexpr u64 mod() const { return m; }
+    constexpr u64 mod(u128 x) const {
+        u128 z = u128(u64(x)) * iml;
+        z = u128(u64(x)) * imh + (x >> 64) * iml + (z >> 64);
+        z = (x >> 64) * imh + (z >> 64);
+        x -= z * m;
+        return x < m ? x : x - m;
+    }
+    constexpr u64 mul(u64 x, u64 y) const { return mod(u128(x) * y); }
 };
