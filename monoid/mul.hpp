5fb6808f1a42d20ef6e1020266388ed4cb4bbd7b
gandyli <andyli20070803@gmail.com>
2023-12-20 01:27:35 +0800

    mul monoid

---
+++ monoid/mul.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+template <typename T>
+struct Monoid_Mul {
+    using value_type = T;
+    using X = value_type;
+    static constexpr X op(const X& a, const X& b) { return a * b; }
+    static constexpr X inverse(const X& x) { return 1 / x; }
+    static constexpr X unit() { return 1; }
+    static constexpr bool commute = true;
+};
