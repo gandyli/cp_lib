0cdc7e90923f0db06d70b3953ffc6138f4a0be2f
gandyli <andyli20070803@gmail.com>
2023-12-20 01:16:03 +0800

    max_idx, min_idx monoid

---
+++ monoid/min_idx.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "template.hpp"
+
+template <typename T, bool less = true>
+struct Monoid_Min_Idx {
+    using value_type = std::pair<T, int>;
+    using X = value_type;
+    static constexpr X op(const X& a, const X& b) {
+        if (a.first < b.first)
+            return a;
+        if (a.first > b.first)
+            return b;
+        if constexpr (less)
+            return a.second < b.second ? a : b;
+        else
+            return a.second > b.second ? a : b;
+    }
+    static constexpr X unit() { return {inf<T>, -1}; }
+    static constexpr bool commute = true;
+};
