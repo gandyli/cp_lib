254a992d92d49732d076f535cc44640f3cf613ef
gandyli <andyli20070803@gmail.com>
2023-12-20 01:21:44 +0800

    maxmaxcnt, minmincnt monoid

---
+++ monoid/maxmaxcnt.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "template.hpp"
+
+template <typename T>
+struct Monoid_MaxMaxcnt {
+    using value_type = std::pair<T, T>;
+    using X = value_type;
+    static constexpr X op(const X& a, const X& b) {
+        if (a.first > b.first)
+            return a;
+        if (a.first < b.first)
+            return b;
+        return {a.first, a.second + b.second};
+    }
+    static constexpr X unit() { return {-inf<T>, 0}; }
+    static constexpr bool commute = true;
+};
