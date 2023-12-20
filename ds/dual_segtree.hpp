0c574f7bd6e1e95000e34a8046c268d69333cf4f
gandyli <andyli20070803@gmail.com>
2023-12-20 09:47:34 +0800

    dual segtree

---
+++ ds/dual_segtree.hpp
@@ -0,0 +1,62 @@
+#pragma once
+#include "template.hpp"
+
+template <typename Monoid>
+struct Dual_SegTree {
+    using M = Monoid;
+    using A = M::value_type;
+
+    int n, lg, sz;
+    Vec<A> a;
+    Dual_SegTree() = default;
+    Dual_SegTree(int n) { build(n); }
+
+    void build(int n) {
+        this->n = n;
+        lg = 1;
+        while ((1 << lg) < n)
+            lg++;
+        sz = 1 << lg;
+        a.assign(sz << 1, M::unit());
+    }
+    A get(int i) {
+        i += sz;
+        _for_r (j, 1, lg + 1)
+            push(i >> j);
+        return a[i];
+    }
+    Vec<A> get_all() {
+        _for (i, sz)
+            push(i);
+        return {a.begin() + sz, a.begin() + sz + n};
+    }
+    void apply(int l, int r, const A& x) {
+        if (l == r)
+            return;
+        l += sz, r += sz;
+        if (!M::commute)
+            _for_r (i, 1, lg + 1) {
+                if (((l >> i) << i) != l)
+                    push(l >> i);
+                if (((r >> i) << i) != r)
+                    push((r - 1) >> i);
+            }
+        while (l < r) {
+            if (l & 1)
+                apply(l++, x);
+            if (r & 1)
+                apply(--r, x);
+            l >>= 1, r >>= 1;
+        }
+    }
+
+private:
+    void apply(int i, const A& x) { a[i] = M::op(a[i], x); }
+    void push(int i) {
+        if (a[i] == M::unit())
+            return;
+        apply(i << 1, a[i]);
+        apply(i << 1 | 1, a[i]);
+        a[i] = M::unit();
+    }
+};
---
+++ test/library_checker/range_affine_point_get.test.cpp
@@ -0,0 +1,27 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_affine_point_get"
+
+#include "all.hpp"
+#include "modint/montgomery.hpp"
+#include "ds/dual_segtree.hpp"
+#include "monoid/affine.hpp"
+
+int main() {
+    dR(u32, n, q);
+    using mint = MMInt998244353;
+    dRV(mint, a, n);
+    using M = Monoid_Affine<mint>;
+    Dual_SegTree<M> st(n);
+    _for (q) {
+        dR(u32, t);
+        if (t == 0) {
+            dR(u32, l, r, x, y);
+            st.apply(l, r, {x, y});
+        }
+        else {
+            dR(u32, p);
+            auto [x, y] = st.get(p);
+            print(x * a[p] + y);
+        }
+    }
+    return 0;
+}
