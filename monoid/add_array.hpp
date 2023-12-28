5d88a5e82d5b446d7b9d3b902a7c74b5840d95ac
gandyli <andyli20070803@gmail.com>
2023-12-28 20:57:22 +0800

    add_array monoid

---
+++ monoid/add_array.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include "template.hpp"
+
+template <typename T, int N>
+struct Monoid_AddArray {
+    using value_type = std::array<T, N>;
+    using X = value_type;
+    static constexpr X op(X a, const X& b) {
+        _for (i, N)
+            a[i] += b[i];
+        return a;
+    }
+    static constexpr X inverse(X x) {
+        foreach (x, x)
+            x = -x;
+        return x;
+    }
+    static constexpr X power(X x, auto n) {
+        foreach (x, x)
+            x *= n;
+        return x;
+     }
+    static constexpr X from_element(auto&& x) { return x; }
+    static constexpr X unit() { return {}; }
+    static constexpr bool commute = true;
+};
