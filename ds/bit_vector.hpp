59e03f1edf631c2127de42a8560cb58de358dd69
gandyli <andyli20070803@gmail.com>
2024-01-05 18:41:53 +0800

    wavelet matrix

---
+++ ds/bit_vector.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "template.hpp"
+
+struct Bit_Vector {
+    Vec<std::pair<u32, u32>> a;
+    Bit_Vector(int n): a((n + 63) >> 5) {}
+    void set(int i) { a[i >> 5].first |= 1U << (i & 31); }
+    void reset(int i) { a[i >> 5].first &= ~(1U << (i & 31)); }
+    void build() {
+        _for (i, len(a) - 1)
+            a[i + 1].second = a[i].second + std::popcount(a[i].first);
+    }
+    u32 rank1(int i) const {
+        auto [x, y] = a[i >> 5];
+        return y + std::popcount(x & ((1U << (i & 31)) - 1));
+    }
+    u32 rank1(int l, int r) const { return rank1(r) - rank1(l); }
+    u32 rank0(int i) const { return i - rank1(i); }
+    u32 rank0(int l, int r) const { return rank0(r) - rank0(l); }
+    u32 rank(int i, bool b) const { return b ? rank1(i) : rank0(i); }
+    u32 rank(int l, int r, bool b) const { return b ? rank1(l, r) : rank0(l, r); }
+};
---
+++ ds/wavelet_matrix.hpp
@@ -0,0 +1,85 @@
+#pragma once
+#include "ds/bit_vector.hpp"
+
+template <typename T, bool COMPRESS = true, int LG = -1>
+struct Wavelet_Matrix {
+    using X = std::conditional_t<COMPRESS, int, T>;
+    int n, lg;
+    Vec<Bit_Vector> b;
+    vi pos;
+    Vec<T> c;
+    Wavelet_Matrix() = default;
+    Wavelet_Matrix(const Vec<T>& a) { build(a); }
+    void build0(Vec<X> a) {
+        pos.resize(lg);
+        b.assign(lg, n);
+        Vec<X> a0(n), a1(n);
+        _for_r (d, lg) {
+            int p = 0, q = 0;
+            _for (i, n) {
+                if (a[i] >> d & 1)
+                    a1[q++] = a[i], b[d].set(i);
+                else
+                    a0[p++] = a[i];
+            }
+            pos[d] = p;
+            b[d].build();
+            swap(a, a0);
+            copy(a1.begin(), a1.begin() + q, a.begin() + p);
+        }
+    }
+    void build(const Vec<T>& a) {
+        n = len(a);
+        if constexpr (COMPRESS) {
+            c.reserve(n);
+            vi d(n);
+            foreach (i, argsort(a)) {
+                if (c.empty() || c.back() != a[i])
+                    c.eb(a[i]);
+                d[i] = len(c) - 1;
+            }
+            c.shrink_to_fit();
+            lg = std::bit_width<u64>(Max(len(c) - 1, 1));
+            build0(std::move(d));
+        }
+        else {
+            if constexpr (LG == -1)
+                lg = std::bit_width<u64>(Max(max(a), 1));
+            else
+                lg = LG;
+            build0(a);
+        }
+    }
+    T kth(int l, int r, int k) const {
+        X res = 0;
+        _for_r (d, lg) {
+            int l0 = b[d].rank0(l), r0 = b[d].rank0(r);
+            if (k < r0 - l0)
+                l = l0, r = r0;
+            else
+                res |= X(1) << d, l += pos[d] - l0, r += pos[d] - r0, k -= r0 - l0;
+        }
+        if constexpr (COMPRESS)
+            return c[res];
+        return res;
+    }
+    int count(int l, int r, T x) const {
+        if constexpr (COMPRESS)
+            x = LB(c, x);
+        if (x == 0)
+            return 0;
+        if (x >= X(1) << lg)
+            return r - l;
+        int cnt = 0;
+        _for_r (d, lg) {
+            int l0 = b[d].rank0(l), r0 = b[d].rank0(r);
+            if (x >> d & 1)
+                cnt += r0 - l0, l += pos[d] - l0, r += pos[d] - r0;
+            else
+                l = l0, r = r0;
+        }
+        return cnt;
+    }
+    int count(int l, int r, T x, T y) const { return count(l, r, y) - count(l, r, x); }
+    int freq(int l, int r, T x) const { return count(l, r, x, x + 1); }
+};
