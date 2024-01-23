46ec7646740d3fa61207be1666a2048f13c7f30c
gandyli <andyli20070803@gmail.com>
2024-01-23 15:09:44 +0800

    update mod_sqrt

--- modint/mod_sqrt.hpp
+++ modint/mod_sqrt.hpp
@@ -1,18 +1,16 @@
 #pragma once
 #include "modint/montgomery.hpp"
 
-template <Unsigned T>
-std::make_signed_t<T> mod_sqrt(T a, T p) {
+template <Modint mint>
+std::optional<mint> mod_sqrt(mint a) {
     if (a < 2)
         return a;
-    SetMMod(T, p);
-    mint A = a;
-    T k = (p - 1) >> 1;
-    if (power(A, k) != 1)
-        return -1;
-    mint b = 1, D = 1 - A;
+    auto k = (mint::mod() - 1) >> 1;
+    if (power(a, k) != 1)
+        return std::nullopt;
+    mint b = 1, D = 1 - a;
     while (power(D, k) == 1)
-        ++b, D = b * b - A;
+        ++b, D = b * b - a;
     k++;
     mint f0 = b, f1 = 1, g0 = 1, g1 = 0;
     while (k) {
@@ -21,5 +19,15 @@ std::make_signed_t<T> mod_sqrt(T a, T p) {
         std::tie(f0, f1) = std::pair{f0 * f0 + D * f1 * f1, 2 * f0 * f1};
         k >>= 1;
     }
-    return g0.val();
+    return g0;
+}
+template <Unsigned T>
+std::optional<T> mod_sqrt(T a, T p) {
+    if (a < 2)
+        return a;
+    SetMMod(T, p);
+    auto r = mod_sqrt<mint>(a);
+    if (r)
+        return r->val();
+    return std::nullopt;
 }
--- test/library_checker/sqrt_mod.test.cpp
+++ test/library_checker/sqrt_mod.test.cpp
@@ -6,7 +6,11 @@
 int main() {
     multipleTests([&] {
         dR(u32, n, p);
-        print(mod_sqrt(n, p));
+        auto r = mod_sqrt(n, p);
+        if (r)
+            print(*r);
+        else
+            print("-1");
     });
     return 0;
 }
