da55c0d2a282989b2adbce78b732cd5c916f0237
gandyli <andyli20070803@gmail.com>
2024-02-06 19:26:24 +0800

    Update transpose

--- linalg/transpose.hpp
+++ linalg/transpose.hpp
@@ -1,10 +1,10 @@
 #pragma once
 #include "template.hpp"
 
-template <typename T>
-vvc<T> transpose(const vvc<T>& a) {
+template <typename V>
+vc<V> transpose(const vc<V>& a) {
     int n = len(a), m = len(a[0]);
-    VEC(T, b, m, n);
+    vc<V> b(m, V(n, decltype(a[0][0]){}));
     _for (i, n)
         _for (j, m)
             b[j][i] = a[i][j];
