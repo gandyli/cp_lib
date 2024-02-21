0c6f4d2bf450c2b4bc7602410247b036a8aa5783
gandyli <andyli20070803@gmail.com>
2024-02-21 16:48:13 +0800

    Refactor convolution functions and improve performance

--- poly/convolution.hpp
+++ poly/convolution.hpp
@@ -29,28 +29,37 @@ vc<mint> convolution_ntt(const vc<mint>& a, const vc<mint>& b) {
         x *= iv;
     return s;
 }
-template <Modint mint>
-vc<mint> convolution_garner(const vc<mint>& a, const vc<mint>& b) {
+template <typename T, typename U = T>
+vc<U> convolution_garner(const vc<T>& a, const vc<T>& b) {
     using namespace ArbitraryNTT;
     int n = len(a), m = len(b);
     vc<mint0> a0(n), b0(m);
     vc<mint1> a1(n), b1(m);
     vc<mint2> a2(n), b2(m);
+    auto val = [&](auto&& a) {
+        if constexpr (Modint<T>)
+            return a.val();
+        else
+            return a;
+    };
     _for (i, n) {
-        a0[i] = a[i].val();
-        a1[i] = a[i].val();
-        a2[i] = a[i].val();
+        a0[i] = val(a[i]);
+        a1[i] = val(a[i]);
+        a2[i] = val(a[i]);
     }
     _for (i, m) {
-        b0[i] = b[i].val();
-        b1[i] = b[i].val();
-        b2[i] = b[i].val();
+        b0[i] = val(b[i]);
+        b1[i] = val(b[i]);
+        b2[i] = val(b[i]);
     }
     auto c0 = convolution_ntt(a0, b0);
     auto c1 = convolution_ntt(a1, b1);
     auto c2 = convolution_ntt(a2, b2);
-    vc<mint> r(n + m - 1);
-    crt<u64>(c0, c1, c2, r, w1 % mint::mod(), w2 % mint::mod());
+    vc<U> r(n + m - 1);
+    if constexpr (Modint<T>)
+        crt<u64>(c0, c1, c2, r, w1 % T::mod(), w2 % T::mod());
+    else
+        crt<U>(c0, c1, c2, r, w1, w2);
     return r;
 }
 template <typename R, typename U = f64, bool ROUND = false>
@@ -59,7 +68,7 @@ vc<U> convolution_fft(const vc<R>& a, const vc<R>& b) {
     int n = len(a), m = len(b);
     if (!n || !m)
         return {};
-    if (min(n, m) <= 50)
+    if (min(n, m) <= 60)
         return convolution_naive<R, U>(a, b);
     int k = get_lg(n + m - 1), sz = 1 << k;
     vc<C> c(sz);
@@ -98,8 +107,6 @@ vc<U> convolution(const vc<T>& a, const vc<T>& b) {
     int n = len(a), m = len(b);
     if (!n || !m)
         return {};
-    if (min(n, m) <= 2500)
-        return convolution_naive<T, U>(a, b);
     {
         constexpr i64 LIM = ten(15);
         i64 sa = 0, sb = 0;
@@ -110,25 +117,9 @@ vc<U> convolution(const vc<T>& a, const vc<T>& b) {
         if (i128(sa) * sb <= LIM)
             return convolution_fft<T, U, true>(a, b);
     }
-    vc<mint0> a0(n), b0(m);
-    vc<mint1> a1(n), b1(m);
-    vc<mint2> a2(n), b2(m);
-    _for (i, n) {
-        a0[i] = a[i];
-        a1[i] = a[i];
-        a2[i] = a[i];
-    }
-    _for (i, m) {
-        b0[i] = b[i];
-        b1[i] = b[i];
-        b2[i] = b[i];
-    }
-    auto c0 = convolution_ntt(a0, b0);
-    auto c1 = convolution_ntt(a1, b1);
-    auto c2 = convolution_ntt(a2, b2);
-    vc<U> r(n + m - 1);
-    crt<U>(c0, c1, c2, r, w1, w2);
-    return r;
+    if (min(n, m) <= 900)
+        return convolution_karatsuba<T, U>(a, b);
+    return convolution_garner<T, U>(a, b);
 }
 template <Modint mint>
 vc<mint> convolution(const vc<mint>& a, const vc<mint>& b) {
@@ -137,8 +128,8 @@ vc<mint> convolution(const vc<mint>& a, const vc<mint>& b) {
         return {};
     if constexpr (StaticModint<mint>) {
         constexpr int lvl = __builtin_ctz(mint::mod() - 1);
-        if (n + m - 1 <= (1 << lvl) && min(n, m) > 50)
+        if (n + m - 1 <= (1 << lvl) && min(n, m) > 40)
             return convolution_ntt(a, b);
     }
-    return min(n, m) <= 200 ? convolution_karatsuba(a, b) : convolution_garner(a, b);
+    return min(n, m) <= 340 ? convolution_karatsuba(a, b) : convolution_garner(a, b);
 }
--- poly/convolution_karatsuba.hpp
+++ poly/convolution_karatsuba.hpp
@@ -1,11 +1,11 @@
 #pragma once
 #include "poly/convolution_naive.hpp"
 
-template <typename T>
-vc<T> convolution_karatsuba(const vc<T>& f, const vc<T>& g) {
+template <typename T, typename U = T>
+vc<U> convolution_karatsuba(const vc<T>& f, const vc<T>& g) {
     int n = len(f), m = len(g);
     if (min(n, m) <= 30)
-        return convolution_naive(f, g);
+        return convolution_naive<T, U>(f, g);
     int mi = ceil(max(n, m), 2);
     vc<T> f1, f2, g1, g2;
     if (n < mi)
@@ -20,14 +20,14 @@ vc<T> convolution_karatsuba(const vc<T>& f, const vc<T>& g) {
         g1 = {g.begin(), g.begin() + mi};
         g2 = {g.begin() + mi, g.end()};
     }
-    auto a = convolution_karatsuba(f1, g1);
-    auto b = convolution_karatsuba(f2, g2);
+    auto a = convolution_karatsuba<T, U>(f1, g1);
+    auto b = convolution_karatsuba<T, U>(f2, g2);
     _for (i, len(f2))
         f1[i] += f2[i];
     _for (i, len(g2))
         g1[i] += g2[i];
-    auto c = convolution_karatsuba(f1, g1);
-    vc<T> r(n + m - 1);
+    auto c = convolution_karatsuba<T, U>(f1, g1);
+    vc<U> r(n + m - 1);
     _for (i, len(a))
         r[i] += a[i], c[i] -= a[i];
     _for (i, len(b))
