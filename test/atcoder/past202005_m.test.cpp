820c1e3def1b5c3831c3b0c62a32fa97343d90b5
gandyli <andyli20070803@gmail.com>
2024-04-17 21:29:11 +0800

    Refactor past202005_m.test.cpp

--- test/atcoder/past202005_m.test.cpp
+++ test/atcoder/past202005_m.test.cpp
@@ -10,20 +10,19 @@ int main() {
     dRV(int, a, K);
     foreach (a, a)
         a--;
-    vvi d_a(K);
+    vvi dis(K);
     _for (i, K)
-        d_a[i] = bfs01<int>(g, a[i]).dis;
-    auto d_s = bfs01<int>(g, s).dis;
+        dis[i] = bfs01<int>(g, a[i]).dis;
     VECI(dp, 1 << K, K, inf<int>);
     _for (i, K)
-        dp[1 << i][i] = d_s[a[i]];
+        dp[1 << i][i] = dis[i][s];
     _for (s, 1 << K) {
         _for (i, K) {
             if (dp[s][i] == inf<int>)
                 continue;
             _for (j, K)
                 if (!(s >> j & 1))
-                    chkmin(dp[s | 1 << j][j], dp[s][i] + d_a[i][a[j]]);
+                    chkmin(dp[s | 1 << j][j], dp[s][i] + dis[i][a[j]]);
         }
     }
     print(min(dp.back()));
