ac29d92e57138d1736481793c403ffbef129cb7e
gandyli <andyli20070803@gmail.com>
2024-05-05 14:28:08 +0800

    Refactor HashMap

--- ds/hashmap.hpp
+++ ds/hashmap.hpp
@@ -3,8 +3,7 @@
 
 template <typename K, typename V>
 struct HashMapBase {
-    vc<K> key;
-    vc<V> val;
+    vc<std::pair<K, V>> kv;
     vc<bool> vis;
     struct iterator {
         using difference_type = int;
@@ -13,9 +12,9 @@ struct HashMapBase {
         int i;
         iterator() = default;
         iterator(const HashMapBase<K, V>* mp, int i): mp(mp), i(i) {}
-        value_type operator*() const { return {mp->key[i], mp->val[i]}; }
+        const value_type& operator*() const { return mp->kv[i]; }
         iterator& operator++() {
-            while (++i < len(mp->key) && !mp->vis[i]) {}
+            while (++i < len(mp->kv) && !mp->vis[i]) {}
             return *this;
         }
         iterator operator++(int) {
@@ -25,45 +24,31 @@ struct HashMapBase {
         }
         bool operator==(const iterator& it) const { return i == it.i; }
     };
-    iterator begin() const {
-        int i = 0;
-        while (i < len(key) && !vis[i])
-            i++;
-        return {this, i};
-    }
-    iterator end() const { return {this, len(key)}; }
+    iterator begin() const { return ++iterator{this, -1}; }
+    iterator end() const { return {this, len(kv)}; }
     vc<K> keys() const {
-        vc<K> res;
-        foreach (k, v, *this)
-            res.eb(k);
-        return res;
+        auto&& r = *this | views::keys;
+        return {all(r)};
     }
     vc<V> values() const {
-        vc<V> res;
-        foreach (k, v, *this)
-            res.eb(v);
-        return res;
+        auto&& r = *this | views::values;
+        return {all(r)};
     }
-    vc<std::pair<K, V>> items() const {
-        vc<std::pair<K, V>> res;
-        foreach (k, v, *this)
-            res.eb(k, v);
-        return res;
-    }
+    vc<std::pair<K, V>> items() const { return {begin(), end()}; }
 };
 template <typename K, typename V>
 struct HashMap: HashMapBase<K, V> {
     using Base = HashMapBase<K, V>;
-    using Base::key, Base::val, Base::vis;
+    using Base::kv, Base::vis;
 
     u32 cap, mask, shift;
     HashMap(int n = 0) { build(n); }
     void reserve(int n) {
-        if (n > len(key)) {
+        if (n > len(kv)) {
             HashMap<K, V> mp(n);
-            _for (i, len(key))
+            _for (i, len(kv))
                 if (vis[i])
-                    mp[key[i]] = val[i];
+                    mp[kv[i].first] = kv[i].second;
             *this = std::move(mp);
         }
     }
@@ -73,22 +58,21 @@ struct HashMap: HashMapBase<K, V> {
         int i = index(k);
         if (!vis[i]) {
             vis[i] = true;
-            key[i] = k;
-            val[i] = {};
+            kv[i] = {k, {}};
             cap--;
         }
-        return val[i];
+        return kv[i].second;
     }
     V get(const K& k, V d = {}) const {
         int i = index(k);
-        return vis[i] ? val[i] : d;
+        return vis[i] ? kv[i].second : d;
     }
     bool contains(const K& k) const {
         int i = index(k);
         return vis[i];
     }
     void clear() { build(0); }
-    int size() const { return len(key) / 2 - cap; }
+    int size() const { return len(kv) / 2 - cap; }
 
 private:
     void build(int n) {
@@ -98,14 +82,13 @@ private:
         cap = k / 2;
         mask = k - 1;
         shift = 64 - std::__lg(k);
-        key.resize(k);
-        val.resize(k);
+        kv.resize(k);
         vis.assign(k, false);
     }
-    void extend() { reserve(len(key) * 2); }
+    void extend() { reserve(len(kv) * 2); }
     int index(const K& k) const {
         int i = hash(k) >> shift;
-        while (vis[i] && key[i] != k)
+        while (vis[i] && kv[i].first != k)
             i = (i + 1) & mask;
         return i;
     }
@@ -114,24 +97,23 @@ template <typename V>
 struct HashMap<char, V>: HashMapBase<char, V> {
     using K = char;
     using Base = HashMapBase<K, V>;
-    using Base::key, Base::val, Base::vis;
+    using Base::kv, Base::vis;
 
     int sz{};
-    HashMap(int = 0) { key.resize(128), val.resize(128), vis.resize(128); }
+    HashMap(int = 0) { kv.resize(128), vis.resize(128); }
     void reserve(int) {}
     V& operator[](const K& k) {
         int i = index(k);
         if (!vis[i]) {
             vis[i] = true;
-            key[i] = k;
-            val[i] = {};
+            kv[i] = {k, {}};
             sz++;
         }
-        return val[i];
+        return kv[i].second;
     }
     V get(const K& k, V d = {}) const {
         int i = index(k);
-        return vis[i] ? val[i] : d;
+        return vis[i] ? kv[i].second : d;
     }
     bool contains(const K& k) const {
         int i = index(k);
