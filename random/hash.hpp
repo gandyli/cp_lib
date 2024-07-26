84b42edba749e8b301c98f574abb1d5f51537826
gandyli <andyli20070803@gmail.com>
2024-07-26 15:03:41 +0800

    refactor: Update hash.hpp to use template specialization

--- random/hash.hpp
+++ random/hash.hpp
@@ -1,10 +1,16 @@
 #pragma once
 #include "seed.hpp"
 
-struct Hash {
-    template <typename T>
-    static void hash_combine(u64& seed, const T& v) { seed ^= Hash{}(v) + 0x9e3779b97f4a7c15 + (seed << 12) + (seed >> 4); }
-    template <Integer T>
+template <typename T>
+struct Hash;
+
+template <typename T>
+u64 hash(const T& x) { return Hash<T>{}(x); }
+template <typename T>
+void hash_combine(u64& seed, const T& x) { seed ^= hash(x) + 0x9e3779b97f4a7c15 + (seed << 12) + (seed >> 4); }
+
+template <Integer T>
+struct Hash<T> {
     u64 operator()(const T& x) const {
         if constexpr (sizeof(T) == 16) {
             u64 v{};
@@ -14,24 +20,30 @@ struct Hash {
         }
         return (x + FIXED_RANDOM) * 11995408973635179863ULL;
     }
-    template <typename T1, typename T2>
+};
+template <typename T1, typename T2>
+struct Hash<std::pair<T1, T2>> {
     u64 operator()(const std::pair<T1, T2>& p) const {
         u64 v{};
         hash_combine(v, p.first);
         hash_combine(v, p.second);
         return v;
     }
-    template <typename... Args>
+};
+template <typename... Args>
+struct Hash<std::tuple<Args...>> {
     u64 operator()(const std::tuple<Args...>& t) const {
         u64 v{};
         std::apply([&](auto&&... args) { (hash_combine(v, args), ...); }, t);
         return v;
     }
-    template <input_range R>
+};
+template <input_range R>
+struct Hash<R> {
     u64 operator()(const R& r) const {
         u64 v{};
         foreach (x, r)
             hash_combine(v, x);
         return v;
     }
-} hash;
+};
