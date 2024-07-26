8c4caba95a9a3036b0998aa9ccdf900170e5d789
gandyli <andyli20070803@gmail.com>
2024-07-26 15:03:23 +0800

    refactor: Update lcm function to use HashMap instead of std::map

--- math/lcm.hpp
+++ math/lcm.hpp
@@ -1,11 +1,12 @@
 #pragma once
 #include "math/factorize.hpp"
+#include "ds/hashmap.hpp"
 
 template <typename R>
 R lcm(const vi& a) {
     if (a.empty())
         return 1;
-    std::map<int, int> mp;
+    HashMap<int, int> mp;
     foreach (x, a)
         foreach (p, c, factorize_pair<u32>(x))
             chkmax(mp[p], c);
@@ -18,7 +19,7 @@ template <typename R>
 R lcm(const vi& a, const vi& lpf) {
     if (a.empty())
         return 1;
-    std::map<int, int> mp;
+    HashMap<int, int> mp;
     foreach (x, a)
         foreach (p, c, factorize_pair(x, lpf))
             chkmax(mp[p], c);
