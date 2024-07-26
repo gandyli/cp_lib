b837853d469dabf68b9b45f43ce77aa6eb7aa2aa
gandyli <andyli20070803@gmail.com>
2024-07-26 15:21:03 +0800

    refactor: Optimize gcd.hpp by using __builtin_ctz for counting trailing zeros

--- math/gcd.hpp
+++ math/gcd.hpp
@@ -1,6 +1,18 @@
 #pragma once
 #include "template.hpp"
 
+template <typename T>
+int ctz(T x) {
+    if constexpr (sizeof(T) <= 4)
+        return __builtin_ctz(x);
+    else if constexpr (sizeof(T) == 8)
+        return __builtin_ctzll(x);
+    else if (u64(x))
+        return ctz(u64(x));
+    else
+        return 64 + ctz(x >> 64);
+}
+
 template <typename T, typename U>
 constexpr auto binary_gcd(T _a, U _b) {
     using S = make_signed_t<std::common_type_t<T, U>>;
@@ -9,16 +21,16 @@ constexpr auto binary_gcd(T _a, U _b) {
         return b;
     if (b == 0)
         return a;
-    int i = std::__countr_zero(a);
-    int j = std::__countr_zero(b);
+    int i = ctz(a);
+    int j = ctz(b);
     int k = min(i, j);
     b >>= j;
     while (a != 0) {
         a >>= i;
         S diff = b - a;
-        i = std::__countr_zero(diff);
+        i = ctz(diff);
         chkmin(b, a);
-        a = max(diff, -diff);
+        a = std::abs(diff);
     }
     return b << k;
 }
